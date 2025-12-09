825f8220d5c55b61f71088de10f87c5a3318ecf4
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2025-12-08 19:39:12 -0800

    first commit

---
+++ my-react-store/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+function App() {  
+  return (
+    <>
+      <div>
+        <Navbar />
+       <h1>Hello from React!</h1>
+       {/* please create the footer component
+       and use it here */}
+        <Footer />     
+      </div>  
+         
+    </>
+  )
+}
+export default App
