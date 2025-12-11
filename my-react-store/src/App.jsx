e2796a170275adeded3cb2778812739e106f9d23
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2025-12-10 20:18:10 -0800

    v2

--- my-react-store/src/App.jsx
+++ my-react-store/src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import Catalog from './pages/Catalog';
 
 function App() {  
   return (
     <>
       <div>
         <Navbar />
-       <h1>Hello from React!</h1>
-       {/* please create the footer component
-       and use it here */}
+        <Catalog />      
         <Footer />     
       </div>  
          
---
+++ my-react-store/src/components/QuantityPicker.jsx
@@ -0,0 +1,34 @@
+import "./QuantityPicker.css";
+import { useState } from "react";
+
+function QuantityPicker() {
+
+    const [quantity, setQuantity] = useState(1);
+    //      name      fuction                initial value
+    // quantity = 2; // This won't work in React
+    // We need to use setQuantity to update the value
+    function handleIncrease() {
+        let temporalValue = quantity + 1;
+        setQuantity(temporalValue);
+    }
+    function handleDecrease(){
+        let temporalValue = quantity - 1;
+        if(temporalValue === 0){
+           return;
+        }
+        setQuantity(temporalValue);
+    }
+    // create the handleDecrease function 
+    // prevent the quantity from going below 1 
+    // disable the button
+    
+    return (
+        <div className="quantity-picker">
+            <button onClick={handleDecrease} disabled={quantity === 1}>-</button>
+            <label>{quantity}</label>
+            <button onClick={handleIncrease}>+</button>
+        </div>
+    );
+}
+
+export default QuantityPicker;
